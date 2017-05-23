beaa246dea19f9c61db08dfffd7da6df399c4a15
Erick Martins Ratamero <erickmartins@gmx.net>
2017-05-23 13:54:36 +0100

    changing permissions for video, keeping tmp for debugging

--- generate_video.py
+++ generate_video.py
@@ -1,8 +1,7 @@
-#run this script by typing pymol view.py at the command line in the directory that contains the pdb files you want to view as a movie.
-#edit name appropriately - this is for the standard input_froda_* files
+# run this script by typing pymol view.py at the command line in the directory that contains the pdb files you want to view as a movie.
+# edit name appropriately - this is for the standard input_froda_* files
 
-#this is to view all of the pdb files in your current working directory in number order, using pymol.
-from glob import glob
+# this is to view all of the pdb files in your current working directory in number order, using pymol.
 import sys
 import os
 import multiprocessing
@@ -11,8 +10,7 @@ import subprocess
 
 
 def parsing_video_args(sys_args):
-    parser = argparse.ArgumentParser(description='Generates videos for the most likely movement modes given a folder where the runs are stored.',usage='%(prog)s folder [options]')
-
+    parser = argparse.ArgumentParser(description='Generates videos for the most likely movement modes given a folder where the runs are stored.', usage='%(prog)s folder [options]')
 
     parser.add_argument('--threed',  action='store_true',
                         help='Flag for generating anaglyph stereo movies')
@@ -34,44 +32,39 @@ def parsing_video_args(sys_args):
     return args
 
 
-
-
 def call_pymol(command):
     os.system(command)
 
 
-
-
-def gen_video(exec_folder,args,folder):
+def gen_video(exec_folder, args, folder):
     print(folder)
     if args.modes:
-        modelist=[int(x) for x in args.modes]
+        modelist = [int(x) for x in args.modes]
     else:
-        modelist=range(7,12)
+        modelist = range(7, 12)
 
     if args.ecuts:
-        cutlist=[float(x) for x in args.ecuts]
+        cutlist = [float(x) for x in args.ecuts]
     else:
-        cutlist=[1.0, 2.0]
-    modelist=[format(i, '02d') for i in modelist]
-    signals=['pos','neg']
-    jobs=[]
-
+        cutlist = [1.0, 2.0]
+    modelist = [format(i, '02d') for i in modelist]
+    signals = ['pos', 'neg']
+    jobs = []
 
     for cut in cutlist:
         for mode in modelist:
             for sign in signals:
-                filename=folder+"/Run-"+str(cut)+"-mode"+mode+"-"+sign+".mpg"
+                filename = folder + "/Run-"+str(cut)+"-mode"+mode+"-"+sign+".mpg"
                 print (filename)
-                prepare_script(exec_folder,args,filename,cut,mode,sign,folder)
+                prepare_script(exec_folder, args, filename, cut, mode, sign, folder)
                 # Desired pymol commands here to produce and save figures
 
-                currfolder=folder+"/Runs/"+str(cut)+"/Mode"+mode+"-"+sign+"/"
+                currfolder = folder + "/Runs/"+str(cut)+"/Mode"+mode+"-"+sign+"/"
                 if args.threed:
-                    command='pymol -q '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py -- '+currfolder
+                    command = 'pymol -q '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py -- '+currfolder
                 else:
-                    command='pymol -cq '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py -- '+currfolder
-                p = multiprocessing.Process(target=call_pymol,args=(command,))
+                    command = 'pymol -cq '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py -- '+currfolder
+                p = multiprocessing.Process(target=call_pymol, args=(command,))
                 jobs.append(p)
                 p.start()
 
@@ -82,31 +75,31 @@ def gen_video(exec_folder,args,folder):
         for cut in cutlist:
             for mode in modelist:
                 for sign in signals:
-                    filename=folder+"/Run-"+str(cut)+"-mode"+mode+"-"+sign+".mpg"
-                    tmpfolder=filename.rsplit("/",1)[1][:-3]
+                    filename = folder+"/Run-"+str(cut)+"-mode"+mode+"-"+sign+".mpg"
+                    tmpfolder = filename.rsplit("/", 1)[1][:-3]
 
-                    currfolder=folder+"/Runs/"+str(cut)+"/Mode"+mode+"-"+sign+"/"
-                    command=['convert', '-quality',' 100',folder+'/'+tmpfolder+'tmp/*.ppm',filename]
+                    currfolder = folder+"/Runs/"+str(cut)+"/Mode"+mode+"-"+sign+"/"
+                    command = ['convert', '-quality', ' 100', folder+'/'+tmpfolder+'tmp/*.ppm', filename]
                     print(command)
                     subprocess.call(command)
-
 
     for cut in cutlist:
         for mode in modelist:
             if args.combi:
-                filename=folder+"/Run-"+str(cut)+"-mode"+mode+"-"
+                filename = folder+"/Run-"+str(cut)+"-mode"+mode+"-"
                 os.system('cat '+filename+'pos.mpg '+filename+'neg.mpg > '+filename+'combi.mpg')
+                os.system('chmod 744 '+filename+'combi.mpg')
             for sign in signals:
                 # os.system('rm '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py')
-                filename=folder+"/Run-"+str(cut)+"-mode"+mode+"-"+sign+".mpg"
-                os.system('chmod 755 '+filename)
-                tmpfolder=filename.rsplit("/",1)[1][:-3]
-                os.system('rm -r '+folder+'/'+tmpfolder+'tmp/')
+                filename = folder+"/Run-"+str(cut)+"-mode"+mode+"-"+sign+".mpg"
+                os.system('chmod 744 '+filename)
+                tmpfolder = filename.rsplit("/", 1)[1][:-3]
+                # os.system('rm -r '+folder+'/'+tmpfolder+'tmp/')
     return
 
-#cmd.set(full_screen='on')
+# cmd.set(full_screen='on')
 
-### cut below here and paste into script ###
+# cut below here and paste into script ###
 # cmd.set_view('0.979684353,   -0.145519406,   -0.137993217,\
 #      0.135169536,   -0.029161714,    0.990392923,\
 #     -0.148145691,   -0.988925874,   -0.008899692,\
@@ -114,32 +107,30 @@ def gen_video(exec_folder,args,folder):
 #    -14.108730316,  -18.215091705,   66.387222290,\
 #    274.463958740,  421.784942627,  -20.000000000' )
 
-def prepare_script(exec_folder,args,filename,cut,mode,sign,folder):
+
+def prepare_script(exec_folder, args, filename, cut, mode, sign, folder):
 
     # string="cat video_template.py <(echo filename=\'"+filename+"\') video_minimal.py >pymolvideo.py"
     # string='cat video_template.py <(echo \"stereo anaglyph\") <(echo filename=\\"'+filename+'\\") ' +args.video[0]+' video_minimal.py > pymolvideo.py'
 
-
-    string='cat '+exec_folder+'/video_template.py '
+    string = 'cat '+exec_folder+'/video_template.py '
     if args.res:
-        string=string+'<(echo \"cmd.viewport('+str(args.res[0])+','+str(args.res[1])+')\") '
+        string = string+'<(echo \"cmd.viewport('+str(args.res[0])+','+str(args.res[1])+')\") '
     if args.threed:
-        string=string+'<(echo \"cmd.set(\\"stereo_mode\\",10)\") <(echo \"cmd.stereo(\\"on\\")\") '
-    string=string+'<(echo filename=\\"'+filename+'\\") '
+        string = string+'<(echo \"cmd.set(\\"stereo_mode\\",10)\") <(echo \"cmd.stereo(\\"on\\")\") '
+    string = string+'<(echo filename=\\"'+filename+'\\") '
     if args.video:
-         strng=string+args.video[0]+' '
-    string=string+exec_folder+'/video_minimal.py > '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py'
+        string = string+args.video[0]+' '
+    string = string+exec_folder+'/video_minimal.py > '+folder+'/pymolvideo'+str(cut)+mode+sign+'.py'
     os.system("bash -c '{0}'".format(string))
 
 
-
-
-if __name__ == "__main__":#
-    args=parsing_video_args(sys.argv)
-    folder=args.folder[0]
-    exec_folder=sys.argv[0].rsplit("/",1)[0]
+if __name__ == "__main__":
+    args = parsing_video_args(sys.argv)
+    folder = args.folder[0]
+    exec_folder = sys.argv[0].rsplit("/", 1)[0]
     if (exec_folder.endswith(".py")):
-        exec_folder="."
+        exec_folder = "."
     # pymol_test()
     # prepare_script(sys.argv,"t1t.mpg")
-    gen_video(exec_folder,args,folder)
+    gen_video(exec_folder, args, folder)
