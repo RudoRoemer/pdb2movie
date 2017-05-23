ad95f1121ba7c9fc9573f959aa09668abeabcad5
Erick Martins Ratamero <erickmartins@gmx.net>
2017-05-23 16:31:30 +0100

    changed from convert to ffmpeg

--- video_template.py
+++ video_template.py
@@ -1,12 +1,14 @@
-#run this script by typing pymol view.py at the command line in the directory that contains the pdb files you want to view as a movie.
-#edit name appropriately - this is for the standard input_froda_* files
+# run this script by typing pymol view.py at the command line in the directory that contains the pdb files you want to view as a movie.
+# edit name appropriately - this is for the standard input_froda_* files
 from pymol import cmd
-#this is to view all of the pdb files in your current working directory in number order, using pymol.
+# this is to view all of the pdb files in your current working directory in number order, using pymol.
 from glob import glob
+import sys
 # print sys.argv
-folder=sys.argv[1]
-
-prot=folder.rsplit("/",1)[1]
+folder = sys.argv[1]
+prot = folder.rsplit("/", 1)[1]
 lst = glob(folder+"/*.pdb")
 lst.sort()
-for fil in lst: cmd.load(fil,"mov")
+for fil in lst:
+    cmd.load(fil, "mov")
+cmd.spectrum()
