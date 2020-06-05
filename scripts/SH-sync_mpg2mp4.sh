34ebfa57ea7593168ff826d29355a912362824f1
RudoRoemer <r.roemer@warwick.ac.uk>
2020-06-05 12:10:39 +0100

    added a possible "y/n" for overwriting old .mp4 files

--- scripts/SH-sync_mpg2mp4.sh
+++ scripts/SH-sync_mpg2mp4.sh
@@ -3,6 +3,7 @@
 source=${1:-"./"}
 target=${2:-"./"}
 pdb=${3:-"aaaa"}
+overwrite=${4:-"n"}
 
 current=`pwd`
 
@@ -22,11 +23,11 @@ do
 
     for mpg in *-combi.mpg
     do 
-	echo $mpg "--> .mp4"
-	if [ ! -f $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4 ]
+	if [[ ! -f $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4 ]] || [[ $mpg -nt $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4 ]]
 	then
+	echo $mpg "--> .mp4"
 #	    ffmpeg -hide_banner -loglevel panic drawtext="text="`basename $mpg`": fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/4: y=(h-text_h)/4" -i $mpg -vcodec libx264 -crf 28 $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4
-	    ffmpeg -hide_banner -loglevel panic -i $mpg -vcodec libx264 -crf 28 $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4
+	ffmpeg -$overwrite -hide_banner -loglevel panic -i $mpg -vcodec libx264 -crf 28 $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4
 	else
 	    echo $target/$dir/$dir/$dir-`basename $mpg .mpg`.mp4 "already exists --- skipping!"
 	fi
